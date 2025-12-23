ec24ba013630075f2baebfe4c67bc4a23811cca7
rahmath-dev01 <rahmath.dev01@gmail.com>
2025-12-23 16:49:33 +0530

    Loops

--- Basics/Data types/practice2.js
+++ Basics/Data types/practice2.js
@@ -1,38 +1,42 @@
-//Log name, age, and city using different console methods
-let q = "king";
-let t = 20;
-let u = true;
+// //Log name, age, and city using different console methods
+// let q = "king";
+// let t = 20;
+// let u = true;
 
-console.log(q)
-console.info(t)
-console.warn(u)
+// console.log(q)
+// console.info(t)
+// console.warn(u)
 
-//  Use prompt() to take user’s name and show a welcome alert
-let username1 = prompt("what is your name");
-alert("Welcome, "+ username1 + "!");
+// //  Use prompt() to take user’s name and show a welcome alert
+// let username1 = prompt("what is your name");
+// alert("Welcome, "+ username1 + "!");
 
-// Log the type of user’s input
-console.log(typeof username1);//string
-
-
-let age =Number( prompt("Enter age"));// You can convert the input to a number
-console.log(age+5);//input =20 0utput = 205. bcz whenever we add with string it with join not add, so output is 205;
-
-//methods
-let msg = "I love Sheryians";
-console.log(msg.replace("love", "Study at"));
-console.log(msg.slice(2,6));
-console.log(msg.split(""));
+// // Log the type of user’s input
+// console.log(typeof username1);//string
 
 
-//Template string example 
- let name1 = "ZAMA" ;
- console.log(`Hey ${name1},Welcome to JS!`);
+// let age =Number( prompt("Enter age"));// You can convert the input to a number
+// console.log(age+5);//input =20 0utput = 205. bcz whenever we add with string it with join not add, so output is 205;
+
+// //methods
+// let msg = "I love Sheryians";
+// console.log(msg.replace("love", "Study at"));
+// console.log(msg.slice(2,6));
+// console.log(msg.split(""));
 
 
- let y = (5+10) * 2;
- console.log(y)
+// //Template string example 
+//  let name1 = "ZAMA" ;
+//  console.log(`Hey ${name1},Welcome to JS!`);
 
- let username2 = prompt("whats u r name");
- alert("Welcome "+ username2 +"!");
- console.log(`hello ${username2},welcome to our store`);
+
+//  let y = (5+10) * 2;
+//  console.log(y)
+
+//  let username2 = prompt("whats u r name");
+//  alert("Welcome "+ username2 +"!");
+//  console.log(`hello ${username2},welcome to our store`);
+
+ let username3 = prompt("whats u r name");
+ alert("Hi,craving for something u r at right place");
+ console.log(`welcome ${username3},to cravingkill`);
--- Basics/Loops/loops.js
+++ Basics/Loops/loops.js
@@ -1,49 +1,25 @@
 // Why Loops?
-
 // Loops help us repeat code without rewriting it.
-
 // If a task needs to be done multiple times (e.g., printing 1-10, going through an array, or
-
 // checking each character in a string), loops are the backbone.
-
-// 28/06/2025, 15:10
-
 // for Loop
-
-
 for (let i = 0; i < 5; i++)
    { console.log(i);
    }
-
 // Start from i = 0
-
 // Run till i < 5
-
 // Increase i each time
-
 // while Loop
 let j = 0;
-
 while (j < 5) {
-
 console.log(j);
-
 j++;
-
 }
-
-
-
 // Condition is checked before running
-
 // do-while Loop
-
 let k = 0;
-
 do
 {
 console.log(k);
-
 k++;
-
 } while (k < 5) ;
