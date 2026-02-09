db25cc3631d6d21766c4d29d8dbcc7870d806915
rahmath-dev01 <rahmath.dev01@gmail.com>
2026-02-09 15:34:23 +0530

    Bulb

---
+++ DOM/Bulb/script.js
@@ -0,0 +1,16 @@
+let btn = document.querySelector('button')
+let box = document.querySelector('div')
+
+let flag = 0
+btn.addEventListener('click',function(){
+ if(flag === 0 ){
+  box.style.backgroundColor = 'yellow'
+  btn.innerHTML = 'OFF'
+  flag = 1
+ }
+ else{
+  box.style.backgroundColor = 'white'
+  btn.innerHTML = 'ON'
+  flag =0 
+ }
+})
