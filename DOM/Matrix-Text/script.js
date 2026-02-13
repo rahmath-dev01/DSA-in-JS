8b7e29627b9758387943076bbb03b7425d516bf8
rahmath-dev01 <rahmath.dev01@gmail.com>
2026-02-14 04:37:37 +0530

    MATRIX_TEXT

--- DOM/Matrix-Text/script.js
+++ DOM/Matrix-Text/script.js
@@ -1,5 +1,21 @@
 const para = document.querySelector('p')
-
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+const txt = para.innerHTML
 para.addEventListener('mouseenter', () => {
+  
+  
+  const A = setInterval(()=> {
+    
+    const str = txt.split('').map((char, index)=>{
+      return characters.split("")[Math.floor(Math.random()*53)]
+      
+    }).join('')
+    
+    para.innerHTML =str
+  },30)
 
+  setTimeout((  )=>{
+    clearInterval(txt)
+
+},100)
 })
