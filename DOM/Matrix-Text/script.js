5110aaac0a86e3c6f9ba07b22b215234844c628f
rahmath-dev01 <rahmath.dev01@gmail.com>
2026-02-14 06:55:39 +0530

    MATRIX_TEXT done

--- DOM/Matrix-Text/script.js
+++ DOM/Matrix-Text/script.js
@@ -1,21 +1,22 @@
 const para = document.querySelector('p')
 const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 const txt = para.innerHTML
-para.addEventListener('mouseenter', () => {
+
+let iteration =0
+
+
+  function randomtext(){
+  const str = txt.split("").map((char,index)=>{
+    if(index < iteration ){
+      return char 
+    }
+    return characters.split("")[Math.floor(Math.random() *52)]
+  }).join("")
+
+  para.innerText = str
+  iteration += 0.25
+}
+setInterval(randomtext , 30)
+
   
   
-  const A = setInterval(()=> {
-    
-    const str = txt.split('').map((char, index)=>{
-      return characters.split("")[Math.floor(Math.random()*53)]
-      
-    }).join('')
-    
-    para.innerHTML =str
-  },30)
-
-  setTimeout((  )=>{
-    clearInterval(txt)
-
-},100)
-})
