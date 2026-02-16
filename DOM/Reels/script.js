327f5b837b2ef838bba4b1aec3e39882c072a3a3
rahmath-dev01 <rahmath.dev01@gmail.com>
2026-02-16 23:58:20 +0530

    Reels completed

--- DOM/Reels/script.js
+++ DOM/Reels/script.js
@@ -1,17 +1,14 @@
 
-
-
-
 const reels = [
   {
     ismuted: true,
-    username: "codewithayush",
+    username: "Notacookofficial",
     likeCount: 14820,
     isLiked: false,
     commentCount: 423,
     shareCount: 92,
     isFollowed: false,
-    caption: "Dark mode > light mode. Change my mind.",
+    caption: "Potato Net Cut Fry 🍟",
     video: "./reels/video01.mp4",
     userprofile: "https://images.unsplash.com/photo-1613915617430-8ab0fd7c6baf?q=80&w=930&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
@@ -24,19 +21,19 @@ const reels = [
     shareCount: 41,
     isFollowed: false,
     caption: "Satisfying Dice Roll and Drop Perfect Loop.",
-    video: "./reels/video06.mp4",
+    video: "./reels/video03.mp4",
     userprofile: "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79"
   },
   {
     ismuted: true,
-    username: "Jadbourhani",
+    username: "Amedits",
     likeCount: 22150,
     isLiked: false,
     commentCount: 612,
     shareCount: 138,
     isFollowed: true,
-    caption: "Range Rover Sport SV | MOST POWERFUL SUV EVER",
-    video: "./reels/video04.mp4",
+    caption: "Defender Edit 🔥",
+    video: "./reels/video10.mp4",
     userprofile: "https://images.unsplash.com/photo-1552374196-c4e7ffc6e126"
   },
   {
@@ -47,8 +44,8 @@ const reels = [
     commentCount: 822,
     shareCount: 201,
     isFollowed: false,
-    caption: "My solo Bali trip changed everything 🌴",
-    video: "./reels/video4.mp4",
+    caption: "Super crispy potato spring 3.0 🥔",
+    video: "./reels/video04.mp4",
     userprofile: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e"
   },
   {
@@ -60,7 +57,7 @@ const reels = [
     shareCount: 55,
     isFollowed: true,
     caption: "Code. Sleep. Repeat. That’s the cycle.",
-    video: "./reels/video5.mp4",
+    video: "./reels/video05.mp4",
     userprofile: "https://images.unsplash.com/photo-1599566150163-29194dcaad36"
   },
   {
@@ -71,8 +68,8 @@ const reels = [
     commentCount: 540,
     shareCount: 87,
     isFollowed: true,
-    caption: "No gym? No problem. Do this 12-min workout at home.",
-    video: "./reels/video1.mp4",
+    caption: "Satisfying Dice Roll and Drop Perfect Loop.",
+    video: "./reels/video06.mp4",
     userprofile: "https://images.unsplash.com/photo-1595152772835-219674b2a8a6"
   },
   {
@@ -83,8 +80,8 @@ const reels = [
     commentCount: 1304,
     shareCount: 412,
     isFollowed: false,
-    caption: "You won’t believe this burger exists 🤯🍔",
-    video: "./reels/video2.mp4",
+    caption: "Range Rover Sport SV | MOST POWERFUL SUV EVER",
+    video: "./reels/video07.mp4",
     userprofile: "https://images.unsplash.com/photo-1552058544-f2b08422138a"
   },
   {
@@ -96,7 +93,7 @@ const reels = [
     shareCount: 73,
     isFollowed: true,
     caption: "Late night vibes // piano version 🎹✨",
-    video: "./reels/video3.mp4",
+    video: "./reels/video08.mp4",
     userprofile: "https://images.unsplash.com/photo-1502685104226-ee32379fefbe"
   },
   {
@@ -108,7 +105,7 @@ const reels = [
     shareCount: 120,
     isFollowed: false,
     caption: "The most underrated smartphone of 2024 📱",
-    video: "./reels/video4.mp4",
+    video: "./reels/video09.mp4",
     userprofile: "https://images.unsplash.com/photo-1511367461989-f85a21fda167"
   },
   {
@@ -119,8 +116,8 @@ const reels = [
     commentCount: 350,
     shareCount: 92,
     isFollowed: true,
-    caption: "Super crispy potato spring 🥔 ",
-    video: "./reels/video04.mp4",
+    caption: "Match the shape ",
+    video: "./reels/video02.mp4",
     userprofile: "https://images.unsplash.com/photo-1494790108377-be9c29b29330"
   }
 ];
@@ -154,7 +151,7 @@ function addData() {
               <h6>${elem.commentCount}</h6>
             </div>
             <div class="share">
-              <h4 class="share-icon icon"><i class="ri-share-forward-line"></i></h4>
+              <h4 class="share-icon icon"><i class="ri-share-forward-line"></i></h4>  
               <h6>${elem.shareCount}</h6>
             </div>
             <div class="menu">
